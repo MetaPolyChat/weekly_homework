889c3949e221d1b10da7ff0a0922bac774eb66a9
syamcat <kdh1998kdh@gmail.com>
2024-08-06 14:18:51 +0900

    first commit

---
+++ src/test/java/com/ohgiraffers/polychat/PolychatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.polychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolychatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
